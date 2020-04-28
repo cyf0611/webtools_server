36ea94227f00e47ad9e3e38a089f128e739ef360
cyf0611 <cao942789776@gmail.com>
2020-04-28 09:50:23 +0800

    none

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -14,7 +14,8 @@ exports.followingTopicContributions = (req, res) => {
 
 exports.watermark = (req, res) => {
     function getWatermarkApi(url) {
-        return `http://wx.qq-5.com/app/index.php?i=2&t=0&v=2.0&from=wxapp&c=entry&a=wxapp&do=query&m=tommie_duanshiping&sign=4ebafe13731541e89d27b3666a13ec1e&url=${url}`;
+        //return `http://wx.qq-5.com/app/index.php?i=2&t=0&v=2.0&from=wxapp&c=entry&a=wxapp&do=query&m=tommie_duanshiping&sign=4ebafe13731541e89d27b3666a13ec1e&url=${url}`;
+        return `https://shuiyin.cyf0611.top/app/index.php?i=3&t=0&v=2.0&from=wxapp&c=entry&a=wxapp&do=query&state=we7sid-fae43caae3e6e5282abf50386a6ad67c&m=tommie_duanshiping&sign=fe42bfd89ac5fc37577f09ee6d080343&url=${url}`;
     }
     let url = req.query && req.query.url;
     mysql.logs({
